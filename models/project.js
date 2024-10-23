30034be7f29ec7f7e98d3b757fbe484f47691460
cc23323 <cc23323@g.unicamp.br>
2024-10-23 12:40:48 -0300

    Implementando a função de salvar projetos no banco de dados (INCOMPLETO)

--- models/project.js
+++ models/project.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 
+// Definir um schema para as faixas, se necessário
+const TrackSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    duration: { type: Number, required: true }, // Duração em segundos
+    // Adicione outros campos nescessário para cada faixa
+})
+
+// Definir o schema do projeto
 const ProjectSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
     name: {type: String, required: true},
     bpm: {type: Number, default: 130},
-    instruments: {type: Object},
-    tracks: {type: Object}
+    instruments: {type: [String], required: true},
+    tracks: {type: [TrackSchema], default: [] },
+    createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Project', ProjectSchema);
